f3890a17cc92026d214e2eb1f0516abfb5fd3b62
JAMWDC <martin.josea@gmail.com>
2025-11-15 17:00:47 +0100

    Fix: Configurando base path para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  // ↓↓↓ AÑADIDO: Se define la ruta base para el despliegue en GitHub Pages ↓↓↓
+  base: '/plan-menu/', 
+  
   plugins: [react()], 
   server: {
     host: '0.0.0.0', 
